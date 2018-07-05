69a2f8be910cca18bcdc9eacc1fae43bc14b0f77
Laurent Magnin <laurent.magnin@logimethods.com>
2018-07-05 14:03:11 -0400

    Setting up MongoDB
    https://docs.openshift.com/container-platform/3.9/using_images/db_images/mongodb.html

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -16,12 +16,25 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 
 # To be Implemented by Student
 
-# https://docs.openshift.com/container-platform/3.9/dev_guide/builds/build_strategies.html#jenkinsfile
-#oc replace -f https://github.com/laugimethods/advdev_homework_template/tree/master/Infrastructure/templates/dev-pipeline.yaml
+echo '------ Setting up MongoDB ------'
+oc_project "$GUID" 'parks-dev'
 
-oc_project "$GUID" 'jenkins'
+. credentials/mongodb.sh
+
+# https://docs.openshift.com/container-platform/3.9/using_images/db_images/mongodb.html
+oc new-app \
+#    -e MONGODB_USER="$MONGODB_USER_DEV" \
+#    -e MONGODB_PASSWORD="$MONGODB_PASSWORD_DEV" \
+#    -e MONGODB_DATABASE="$MONGODB_DATABASE_DEV" \
+#    -e MONGODB_ADMIN_PASSWORD="$MONGODB_ADMIN_PASSWORD_DEV" \
+    mongodb-ephemeral
+exit 0
 
 echo '------ Create dev-pipeline ------'
+oc_project "$GUID" 'jenkins'
+
+# https://docs.openshift.com/container-platform/3.9/dev_guide/builds/build_strategies.html#jenkinsfile
+#oc replace -f https://github.com/laugimethods/advdev_homework_template/tree/master/Infrastructure/templates/dev-pipeline.yaml
 oc create -f ../templates/dev-pipeline.yaml
 oc set env buildconfigs/dev-pipeline GUID="$GUID"
 oc set env buildconfigs/dev-pipeline CLUSTER="$CLUSTER"
