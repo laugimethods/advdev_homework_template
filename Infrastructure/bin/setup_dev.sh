edaa8471d4e648b55c77697af563ed009eb917cf
Laurent Magnin <laurent.magnin@logimethods.com>
2018-09-05 10:45:45 -0400

    env dc/mlbparks --from configmap/mlbparks-config configmap/mongodb-config

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -21,29 +21,40 @@ oc_project "$GUID" 'parks-dev'
 echo '------ Setting up the DEV project ------'
 oc policy add-role-to-user edit "system:serviceaccount:${GUID}-jenkins:jenkins"
 
-oc new-build --binary=true --name="mlbparks" jboss-eap70-openshift:1.6
+echo '------ Setting up the ephemeral MongoDB ------'
+source ./configs/mongodb_dev.sh
+# https://docs.openshift.com/container-platform/3.9/using_images/db_images/mongodb.html
+oc new-app \
+    -p MONGODB_USER="$MONGODB_USERNAME" \
+    -p MONGODB_PASSWORD="$MONGODB_PASSWORD" \
+    -p MONGODB_DATABASE="$MONGODB_DATABASE" \
+    -p MONGODB_ADMIN_PASSWORD="$MONGODB_ADMIN_PASSWORD" \
+    --name="$MONGODB_NAME" \
+    mongodb-ephemeral
+
+oc create configmap mongodb-config \
+    --from-literal DB_HOST="$MONGODB_HOST" \
+    --from-literal DB_PORT="$MONGODB_PORT" \
+    --from-literal DB_NAME="$MONGODB_NAME" \
+    --from-literal DB_DATABASE="$MONGODB_DATABASE" \
+    --from-literal DB_USERNAME="$MONGODB_USERNAME" \
+    --from-literal DB_PASSWORD="$MONGODB_PASSWORD" \
+    --from-literal DB_REPLICASET="$MONGODB_REPLICASET"
+
+echo '------ Setting up the MLB Parks Application ------'
+oc new-build --binary=true --name="mlbparks" jboss-eap70-openshift:1.7
+
+oc create configmap mlbparks-config \
+  --from-literal="APPNAME=MLB Parks (Dev)"
 oc new-app "${GUID}-parks-dev/mlbparks:0.0-0" --name=mlbparks --allow-missing-imagestream-tags=true
+oc set env dc/mlbparks --from configmap/mlbparks-config configmap/mongodb-config
+
 oc set triggers dc/mlbparks --remove-all
 oc expose dc mlbparks --port 8080
 oc expose svc mlbparks
-oc create configmap mlbparks-config --from-literal="APPNAME=MLB Parks (Dev)"
+
 ##oc set volume dc/tasks --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=tasks-config -n xyz-tasks-dev
 ##oc set volume dc/tasks --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=tasks-config -n xyz-tasks-dev
-
-echo '------ Setting up MongoDB ------'
-
-source ./credentials/mongodb_dev.sh
-# https://docs.openshift.com/container-platform/3.9/using_images/db_images/mongodb.html
-oc new-app \
-    -p MONGODB_USER="$MONGODB_USER_DEV" \
-    -p MONGODB_PASSWORD="$MONGODB_PASSWORD_DEV" \
-    -p MONGODB_DATABASE="$MONGODB_DATABASE_DEV" \
-    -p MONGODB_ADMIN_PASSWORD="$MONGODB_ADMIN_PASSWORD_DEV" \
-    mongodb-ephemeral
-
-echo '-------- ConfigMap Creation -----------'
-oc delete configmap mlbparks-config --ignore-not-found=true
-oc create configmap mlbparks-config --from-file=../templates/parks-dev.properties
 
 echo '------ Start dev-pipeline ------'
 #oc_project "$GUID" 'jenkins'
