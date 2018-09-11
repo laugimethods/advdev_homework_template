22cd670e0a17441c969eb9989ba51f364c62e8f8
Laurent Magnin <laurent.magnin@logimethods.com>
2018-09-11 11:49:24 -0400

    Scale up/down

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -20,9 +20,29 @@ echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Gr
 curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/appname/"
 curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/backends/list"
 
+# Scale up mlbparks-green
+oc scale dc/mlbparks-green --replicas=1 -n "${GUID}-parks-prod"
+oc rollout status dc/mlbparks-green -n "${GUID}-parks-prod"
+# Update the route to mlbparks-green
 oc patch route/mlbparks -p '{"spec":{"to":{"name":"mlbparks-green"}}}' -n "${GUID}-parks-prod"
+# Scale down mlbparks-blue
+oc scale dc/mlbparks-blue --replicas=0 -n "${GUID}-parks-prod"
+
+# Scale up nationalparks-green
+oc scale dc/nationalparks-green --replicas=1 -n "${GUID}-parks-prod"
+oc rollout status dc/nationalparks-green -n "${GUID}-parks-prod"
+# Update the route to nationalparks-green
 oc patch route/nationalparks -p '{"spec":{"to":{"name":"nationalparks-green"}}}' -n "${GUID}-parks-prod"
+# Scale down mlbparks-blue
+oc scale dc/nationalparks-blue --replicas=0 -n "${GUID}-parks-prod"
+
+# Scale up parksmap-green
+oc scale dc/parksmap-green --replicas=1 -n "${GUID}-parks-prod"
+oc rollout status dc/parksmap-green -n "${GUID}-parks-prod"
+# Update the route to parksmap-green
 oc patch route/parksmap -p '{"spec":{"to":{"name":"parksmap-green"}}}' -n "${GUID}-parks-prod"
+# Scale down parksmap-blue
+oc scale dc/parksmap-blue --replicas=0 -n "${GUID}-parks-prod"
 
 curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/appname/"
 curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/backends/list"
