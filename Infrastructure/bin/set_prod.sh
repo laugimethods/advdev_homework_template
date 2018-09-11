5402093fb60ff8799fd6123e1523460c53b66b0c
Laurent Magnin <laurent.magnin@logimethods.com>
2018-09-11 16:54:56 -0400

    switch_all_service_color()

--- Infrastructure/bin/set_prod.sh
+++ Infrastructure/bin/set_prod.sh
@@ -8,57 +8,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
-echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Green Services"
+echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Blue Services"
 
-# Code to reset the parks production environment to make
-# all the green services/routes active.
-# This script will be called in the grading pipeline
-# if the pipeline is executed without setting
-# up the whole infrastructure to guarantee a Blue
-# rollout followed by a Green rollout.
-
-curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/appname/"
-echo ""
-curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/backends/list"
-echo ""
-
-# Scale up mlbparks-blue
-oc scale dc/mlbparks-blue --replicas=1 -n "${GUID}-parks-prod"
-oc rollout latest dc/mlbparks-blue -n "${GUID}-parks-prod"
-oc rollout status dc/mlbparks-blue -n "${GUID}-parks-prod"
-curl "http://mlbparks-blue-${GUID}-parks-prod.apps.${CLUSTER}/ws/info/"
-# Update the route to mlbparks-blue
-oc patch route/mlbparks -p '{"spec":{"to":{"name":"mlbparks-blue"}}}' -n "${GUID}-parks-prod"
-# Scale down mlbparks-green
-oc scale dc/mlbparks-green --replicas=0 -n "${GUID}-parks-prod"
-
-# Scale up nationalparks-blue
-oc scale dc/nationalparks-blue --replicas=1 -n "${GUID}-parks-prod"
-oc rollout latest dc/nationalparks-blue -n "${GUID}-parks-prod"
-oc rollout status dc/nationalparks-blue -n "${GUID}-parks-prod"
-curl "http://nationalparks-blue-${GUID}-parks-prod.apps.${CLUSTER}/ws/info/"
-# Update the route to nationalparks-blue
-oc patch route/nationalparks -p '{"spec":{"to":{"name":"nationalparks-blue"}}}' -n "${GUID}-parks-prod"
-# Scale down nationalparks-green
-oc scale dc/nationalparks-green --replicas=0 -n "${GUID}-parks-prod"
-
-# Scale up parksmap-blue
-oc scale dc/parksmap-blue --replicas=1 -n "${GUID}-parks-prod"
-oc rollout latest dc/parksmap-blue -n "${GUID}-parks-prod"
-oc rollout status dc/parksmap-blue -n "${GUID}-parks-prod"
-curl "http://parksmap-blue-${GUID}-parks-prod.apps.${CLUSTER}/ws/appname/"
-echo ""
-curl "http://parksmap-blue-${GUID}-parks-prod.apps.${CLUSTER}/ws/backends/list"
-echo ""
-# Update the route to parksmap-blue
-oc patch service/parksmap \
-  -p '{"metadata":{"labels":{"app":"parksmap-blue", "template":"parksmap-blue"}},
-       "spec":{"selector":{"app":"parksmap-blue", "deploymentconfig":"parksmap-blue"}}}' \
-  -n "${GUID}-parks-prod"
-# Scale down parksmap-green
-oc scale dc/parksmap-green --replicas=0 -n "${GUID}-parks-prod"
-
-curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/appname/"
-echo ""
-curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/backends/list"
-echo ""
+source ./utils.sh
+switch_all_service_color 'green' 'blue' "$GUID"
--- Infrastructure/bin/utils.sh
+++ Infrastructure/bin/utils.sh
@@ -11,3 +11,54 @@ oc_project () {
     sleep 5
   done
 }
+
+switch_service_color() {
+  SERVICE=$1
+  CURRENT=$2
+  TARGET=$3
+  GUID=$4
+
+  echo ""
+  echo "Setting ${SERVICE} Service in Parks Production Environment in project ${GUID}-parks-prod from ${CURRENT} to ${TARGET}"
+  echo ""
+
+  # Scale up TARGET SERVICE
+  oc scale dc/${SERVICE}-${TARGET} --replicas=1 -n "${GUID}-parks-prod"
+  oc rollout latest dc/${SERVICE}-${TARGET} -n "${GUID}-parks-prod"
+  oc rollout status dc/${SERVICE}-${TARGET} -n "${GUID}-parks-prod"
+  #  curl "http://${SERVICE}-${TARGET}-${GUID}-parks-prod.apps.${CLUSTER}/ws/appname/"
+  #  echo ""
+  #  curl "http://${SERVICE}-${TARGET}-${GUID}-parks-prod.apps.${CLUSTER}/ws/backends/list"
+  #  echo ""
+  # Update the route to ${SERVICE}-${TARGET}
+  oc patch service/${SERVICE} \
+    -p "{\"metadata\":{\"labels\":{\"app\":\"${SERVICE}-${TARGET}\", \"template\":\"${SERVICE}-${TARGET}\"}}, \
+    \"spec\":{\"selector\":{\"app\":\"${SERVICE}-${TARGET}\", \"deploymentconfig\":\"${SERVICE}-${TARGET}\"}}}" \
+    -n "${GUID}-parks-prod"
+  # Scale down ${SERVICE}-${CURRENT}
+  oc scale dc/${SERVICE}-${CURRENT} --replicas=0 -n "${GUID}-parks-prod"
+}
+
+switch_all_service_color() {
+  CURRENT=$1
+  TARGET=$2
+  GUID=$3
+
+  curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/appname/"
+  echo ""
+  curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/backends/list"
+  echo ""
+
+  switch_service_color 'mlbparks' "${CURRENT}" "${TARGET}" "${GUID}"
+  curl "http://mlbparks-${TARGET}-${GUID}-parks-prod.apps.${CLUSTER}/ws/info/"
+
+  switch_service_color 'nationalparks' "${CURRENT}" "${TARGET}" "${GUID}"
+  curl "http://nationalparks-${TARGET}-${GUID}-parks-prod.apps.${CLUSTER}/ws/info/"
+
+  switch_service_color 'parksmap' "${CURRENT}" "${TARGET}" "${GUID}"
+
+  curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/appname/"
+  echo ""
+  curl "http://parksmap-${GUID}-parks-prod.apps.${CLUSTER}/ws/backends/list"
+  echo ""
+}
