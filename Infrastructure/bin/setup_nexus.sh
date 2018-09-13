61c3bc35045acded8d49614d1c3fbfe68e10cd00
Laurent Magnin <laurent.magnin@logimethods.com>
2018-09-13 15:00:26 -0400

    oc get route nexus3   -n "${GUID}-nexus"

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -46,7 +46,7 @@ echo -n "Configuring Nexus"
 
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
-./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
+./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n "${GUID}-nexus")
 rm setup_nexus3.sh
 
 echo -n "Configuring Nexus Service and Route"
