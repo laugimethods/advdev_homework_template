56bef270ac0a2da51eafc11ebb9bc778dc09b5ed
Laurent Magnin <laurent.magnin@logimethods.com>
2018-09-13 15:20:27 -0400

    echo "Connected to master.${CLUSTER}" as "$OPENTLC_UserID" user

--- Infrastructure/bin/oc_connect.sh
+++ Infrastructure/bin/oc_connect.sh
@@ -2,3 +2,4 @@
 . credentials/opentlc.sh
 . credentials/cluster.sh
 oc login -u "$OPENTLC_UserID" -p "$OPENTLC_Password" "master.${CLUSTER}"
+echo "Connected to master.${CLUSTER}" as "$OPENTLC_UserID" user.
